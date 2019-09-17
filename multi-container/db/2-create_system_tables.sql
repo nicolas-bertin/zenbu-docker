0684bc8063d97ad2a0e6fcbc5f64548031edf6e1
Nicolas Bertin <nicolas.bertin@gmail.com>
2019-09-17 17:48:10 +0800

    fix bug in db creation steps add couple optional (DOMAIN, MAILTO, ...) default values in composing services

--- multi-container/db/2-create_system_tables.sql
+++ multi-container/db/2-create_system_tables.sql
@@ -7,6 +7,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+USE `zenbu_users`;
+
 --
 -- Table structure for table `user`
 --
