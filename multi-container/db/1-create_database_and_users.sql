fabafe7aed4fdb759dc2047c55bf45738aa73c65
Nicolas Bertin <nicolas.bertin@gmail.com>
2019-09-20 15:34:05 +0800

    Put datalake-readonly-nfsvolume creation and mounting into a optional docker-compose yml file Resurect the possibilty to alter MYSQL zenbu-admin password Get ride off MYSQL root passwrd (passwd is generated randomly and can be retrieved by consulting zenbu_db logs) Modify README.md accordingly

--- multi-container/db/1-create_database_and_users.sql
+++ multi-container/db/1-create_database_and_users.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE zenbu_users;
+CREATE DATABASE IF NOT EXISTS zenbu_users;
 
-CREATE USER 'zenbu_admin'@'%' IDENTIFIED BY 'zenbu_admin';
+#CREATE USER IF NOT EXISTS 'zenbu_admin'@'%' IDENTIFIED BY 'zenbu_admin';
 GRANT SELECT, CREATE TEMPORARY TABLES, LOCK TABLES, CREATE, ALTER, DELETE, INDEX, INSERT, UPDATE, SHOW DATABASES on *.* to 'zenbu_admin'@"%";
 
 CREATE USER 'read'@'%' IDENTIFIED BY 'read';
